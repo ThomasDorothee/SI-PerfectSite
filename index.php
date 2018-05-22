d35d2f82a6996732c4d5ae7ead4c2e96cbf90cb5
tomdoro <thomas_051998@outlook.fr>
2018-05-22 11:12:21 +0200

    First Commit 🤯

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Une Année de voyage</title>
+  <link rel="stylesheet" href="./src/styles/styles.css">
+  <link rel="stylesheet" href="./src/styles/flexboxgrid.css">
+</head>
+<body>
+
+
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+<script src="./src/script.js"></script>
+</body>
+</html>
